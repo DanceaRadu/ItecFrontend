42ddf6e0eb115feae365e2fd2ba5183830b2797d
DanceaRadu <radu.dancea@gmail.com>
2024-04-06 15:35:20 +0300

    Worked on the statistics page.

---
+++ src/app/components/application/app-page/app-page/app-page.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {Application} from "../../../../entity/Application";
+import {ApplicationService} from "../../../../service/application.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-app-page',
+  templateUrl: './app-page.component.html',
+  styleUrl: './app-page.component.scss'
+})
+export class AppPageComponent implements OnInit{
+
+  application: Application | undefined;
+  id: string | null = null;
+  constructor(
+    private applicationService: ApplicationService,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if(!this.id) return;
+    this.applicationService.getAppById(parseInt(this.id)).subscribe(application => {
+      this.application = application;
+    });
+  }
+}
---
+++ src/app/components/application/app-page/app-reports/app-reports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-reports',
+  templateUrl: './app-reports.component.html',
+  styleUrl: './app-reports.component.scss'
+})
+export class AppReportsComponent {
+
+}
